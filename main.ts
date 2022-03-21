c4077d893329d201226a55cbe72e97715c64778e
Adam O'Brien <74919798+Adam0Brien@users.noreply.github.com>
2022-03-21 20:35:59 +0000

    Motor turns on but not off

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
-radio.onReceivedValue(function (name, value) {
-    microIoT.microIoT_ServoRun(microIoT.aServos.S1, value)
+microIoT.microIoT_MQTT_Event(microIoT.TOPIC.topic_0, function (value) {
+    microIoT.showUserText(0, "Data Recieved")
+    speed = parseFloat(value)
+    microIoT.showUserText(1, convertToText(speed))
+    microIoT.microIoT_ServoRun(microIoT.aServos.S1, speed)
 })
-let humidity = 0
+let speed = 0
 basic.showLeds(`
     . . . . .
     . . . . .
@@ -10,16 +13,27 @@ basic.showLeds(`
     . . . . .
     `)
 microIoT.initDisplay()
-microIoT.microIoT_WIFI("Adams hotspot", "password")
+microIoT.microIoT_WIFI("pbb.ie-2G-8280", "xfpkgqr3")
+basic.showLeds(`
+    . . # . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
 microIoT.microIoT_MQTT(
 "hYn5IyqJh7U9lfVdiSvSyJb1",
 "ExMVG1hBAbrE0MFpyEwJJGJXAlT6zYwr",
-"Motor/data",
+"Motor/Speed",
 microIoT.SERVERS.Global
 )
-let value = 0
+basic.showLeds(`
+    . . # . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . # . .
+    `)
 basic.forever(function () {
-    humidity = pins.analogReadPin(AnalogPin.P1)
-    microIoT.showUserText(0, "humidity:" + humidity)
-    microIoT.microIoT_SendMessage(convertToText(humidity), microIoT.TOPIC.topic_0)
+	
 })
